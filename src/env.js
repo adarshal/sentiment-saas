498d7b4002b8d6475e5d36c41ac4867e628e7084
Adarsh Alagade <adarsh@hoc.com>
2025-02-15 05:53:55 +0530

    pages structure

--- src/env.js
+++ src/env.js
@@ -18,6 +18,7 @@ export const env = createEnv({
       // AWS_REGION:z.string(),
       // AWS_ACCESS_KEY_ID:z.string(),
       // AWS_SECRET_ACCESS_KEY: z.string(),
+      //AWS_INFERENCE_BUCKET: z.string(),
   },
 
   /**
